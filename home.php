4f2ea4899a4fe9380d8c5a16cbfc2597a0d7a35d
diegocanepa <diego.canepa241198@gmail.com>
2019-12-09 16:16:45 -0300

    Perricola
    Mañana te mando un audiaso para que sepas que hacer una paja escribir por aca jajajaj

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php require_once("head.php") ?>
+    <title></title>
+  </head>
+  <body>
+
+
+
+
+  <?php require_once("footer.php") ?>
+  </body>
+</html>
